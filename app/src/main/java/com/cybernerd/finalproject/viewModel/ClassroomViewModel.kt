5759d40b3f022d076067bb205ce408c2b7805db4
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-11 05:19:48 +0530

    viewmodel to fetch from repository

--- app/src/main/java/com/cybernerd/finalproject/viewModel/ClassroomViewModel.kt
+++ app/src/main/java/com/cybernerd/finalproject/viewModel/ClassroomViewModel.kt
@@ -1,17 +1,31 @@
 package com.cybernerd.finalproject.viewModel
 
 import android.app.Application
+import android.se.omapi.Session
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.cybernerd.finalproject.model.ClassroomResponse
 import com.cybernerd.finalproject.repository.ClassroomHomeRepository
+import com.cybernerd.finalproject.utils.SessionManager
 
-class ClassroomViewModel(application: Application) : AndroidViewModel(application){
 
-    val classroomlist : LiveData<ClassroomResponse>
-    val repository = ClassroomHomeRepository(application)
+class ClassroomViewModel(application: Application): AndroidViewModel(application){
+
+    lateinit var repository : ClassroomHomeRepository
+    val classroomsLiveData : LiveData<ClassroomResponse>
 
     init {
-        this.classroomlist = repository.classroom
+
+        repository = SessionManager(application).fetchAuthToken()?.let {
+            ClassroomHomeRepository(it)
+        }!!
+
+        this.classroomsLiveData = repository.classroomLiveData
+
+    }
+
+    fun getAllClassroom(){
+        repository.getAllClassroom()
     }
 }
+
--- app/src/main/java/com/cybernerd/finalproject/viewModel/LoginActivityViewModel.kt
+++ app/src/main/java/com/cybernerd/finalproject/viewModel/LoginActivityViewModel.kt
@@ -3,30 +3,46 @@ package com.cybernerd.finalproject.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.cybernerd.finalproject.model.LoginResponse
 import com.cybernerd.finalproject.repository.ClassroomHomeRepository
 import com.cybernerd.finalproject.repository.LoginActivityRepository
+import com.cybernerd.finalproject.utils.SessionManager
 
-class LoginActivityViewModel(application: Application): AndroidViewModel(application) {
+class LoginActivityViewModel(application: Application): AndroidViewModel(application), LoginActivityRepository.LoginActivityResult {
+
 
 
     private val repository = LoginActivityRepository(application)
     val showprogress : LiveData<Boolean>
-    val loginData : LiveData<LoginResponse>
-    val classroomRepository = ClassroomHomeRepository(application)
-    val tokenData : LiveData<String>
+    val loginData : MutableLiveData<LoginResponse>
+
+    lateinit var classroomRepository : ClassroomHomeRepository
+
 
     init {
         this.showprogress = repository.showProgress
         this.loginData = repository.loginData
-        this.tokenData = classroomRepository.token
+
+        classroomRepository = SessionManager(application).fetchAuthToken()?.let {
+            ClassroomHomeRepository(
+                it
+            )
+        }!!
     }
 
     fun login(email: String, password: String){
-        repository.login(email,password)
+        repository.login(email,password, this)
     }
 
-    fun getToken(token: String){
-        classroomRepository.token.value = token
+
+    override fun onSuccess(response: LoginResponse) {
+        loginData.value = response
+        classroomRepository.getAllClassroom()
+
+    }
+
+    override fun onFailer(t: Throwable) {
+
     }
 }
