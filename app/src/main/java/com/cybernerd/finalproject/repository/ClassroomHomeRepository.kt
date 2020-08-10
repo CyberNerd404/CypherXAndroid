94f076d19ba592741de5aae6627f8588eb12fd03
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-10 10:45:25 +0530

    get classrooms by passing Token

--- app/src/main/java/com/cybernerd/finalproject/repository/ClassroomHomeRepository.kt
+++ app/src/main/java/com/cybernerd/finalproject/repository/ClassroomHomeRepository.kt
@@ -11,17 +11,19 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class ClassroomHomeRepository(val application: Application) {
 
-    val token = MutableLiveData<String>()
+
+class ClassroomHomeRepository(val token: String) {
+
+
     val classroom = MutableLiveData<ClassroomResponse>()
 
 
     fun getAllClassroom(){
 
-        CypherXAPI().getClassroom("$token").enqueue(object : Callback<ClassroomResponse>{
+        CypherXAPI().getClassroom(token).enqueue(object : Callback<ClassroomResponse>{
             override fun onFailure(call: Call<ClassroomResponse>, t: Throwable) {
-                Toast.makeText(application,"Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                debug("token","${t.message}")
             }
 
             override fun onResponse(
